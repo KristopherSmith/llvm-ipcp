ea9b74ba5cedee4a02fa234c19acab6d76e59e4c
Moeiz Riaz <moeizr@umich.edu>
2015-12-06 13:50:48 -0500

    adding more files

---
+++ ipco/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+make
+clang -emit-llvm -o test/test.bc -c test/test.c
+
+llc test/test.bc -o test/test.s
+g++ -o test/test test/test.s 
+./test/test
+
+opt -f -load ../lib/IPCO.so  -ipconstprop  test/test.bc > test/test.slicm.bc
+
+#llc test/test.slicm.bc -o test/test.slicm.s
+#g++ -o test/test test/test.slicm.s 
+
+#./test/test
+
+
